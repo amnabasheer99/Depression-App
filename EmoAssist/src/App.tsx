1642c8565ff3353c656d6122b89792a55ee271c2
devsumarjunaid <145253338+SudoW1n@users.noreply.github.com>
2023-12-22 04:02:41 +0530

    Completed Login (There will be some npm installations to be done )

--- EmoAssist/src/App.tsx
+++ EmoAssist/src/App.tsx
@@ -1,75 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import landingImage from "./assets/landingPage.jpg";
-import Chatbot from "./components/Chatbot";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import LandingPage from "./components/LandingPage";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+
+// Import your publishable key
+const PUBLISHABLE_KEY =
+  "pk_test_c2luZ3VsYXItdG9ydG9pc2UtMTguY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 function App() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg  menu shadow fixed-top" id="nau">
-        <div className="container">
-          <a className="navbar-brand " href="#">
-            <img src="" alt="logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="">
-                  <button className="custom-btn btn-11">Login</button>
-                </a>
-              </li>
-            </ul>
-            {/* <button type="button" className="rounded-pill btn-rounded">
-              vaceyra@gmail.com
-              <span>
-                <i className="fa-regular fa-envelope"></i>
-              </span>
-            </button> */}
-          </div>
-        </div>
-      </nav>
-      <section id="landingSection" style={{ marginTop: "0px" }}>
-        <div className="container">
-          <div className="row vh-100 justify-content-center align-items-center">
-            <div className="col-md-12 text-left">
-              <h3 id="landingSubHeading">Welcome to EmoAssist</h3>
-              <h1 id="landingHeading">Break free into yourself</h1>
-              <br />
-              <a id="getStarted" href="">
-                Get Started Now{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="14"
-                  viewBox="0 0 448 513"
-                  color="white"
-                >
-                  <path
-                    d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
-                    fill="white"
-                  />
-                </svg>
-              </a>
-              <Chatbot />
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
+    <Router>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </>
+      </ClerkProvider>
+    </Router>
   );
 }
 
